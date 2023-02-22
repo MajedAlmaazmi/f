46914c7d78f2b0e4f26ae2f452e270d7fb17e1f9
Glitch (messenger-platform-quick-start) <none>
2023-02-22 07:04:07 +0000

    🏂🕋 Checkpoint ./app.js:54941748/9730

--- app.js
+++ app.js
@@ -51,47 +51,9 @@ app.post("/webhook", (req, res) => {
           "/messages?access_token=" +
           token,
         data: {
-          recipient_type: "individual",
+          messaging_product: "whatsapp",
           to: from,
-          type: "interactive",
-          interactive: {
-            type: "list",
-            header: {
-              type: "text",
-              text: "your-header-content",
-            },
-            body: {
-              text: "your-text-message-content",
-            },
-            footer: {
-              text: "your-footer-content",
-            },
-            action: {
-              button: "cta-button-content",
-              sections: [
-                {
-                  title: "your-section-title-content",
-                  rows: [
-                    {
-                      id: "unique-row-identifier",
-                      title: "row-title-content",
-                      description: "row-description-content",
-                    },
-                  ],
-                },
-                {
-                  title: "your-section-title-content",
-                  rows: [
-                    {
-                      id: "unique-row-identifier",
-                      title: "row-title-content",
-                      description: "row-description-content",
-                    },
-                  ],
-                },
-              ],
-            },
-          },
+          text: { body: "Ack: " + msg_body },
         },
         headers: { "Content-Type": "application/json" },
       });
@@ -104,12 +66,12 @@ app.post("/webhook", (req, res) => {
 });
 
 // Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
-// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests 
 app.get("/webhook", (req, res) => {
   /**
    * UPDATE YOUR VERIFY TOKEN
    *This will be the Verify Token value when you set up webhook
-   **/
+  **/
   const verify_token = process.env.VERIFY_TOKEN;
 
   // Parse params from the webhook verification request
